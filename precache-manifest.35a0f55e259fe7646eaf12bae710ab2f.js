129ab2d5d357b938f7d52e87a210bd4575b92905
Mark Koenig <markrkoenig95@gmail.com>
2020-01-24 22:08:42 +0800

    Updates

---
+++ precache-manifest.35a0f55e259fe7646eaf12bae710ab2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc856a1aae15461fcc5ed770d5d8cb91",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "0df3af81d4c862da3b25",
+    "url": "/CV/static/css/main.b47cd4f3.chunk.css"
+  },
+  {
+    "revision": "1c6478e13656063790d1",
+    "url": "/CV/static/js/2.3716d375.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/CV/static/js/2.3716d375.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0df3af81d4c862da3b25",
+    "url": "/CV/static/js/main.a1a74488.chunk.js"
+  },
+  {
+    "revision": "3c34b85aa46cd944f6a5",
+    "url": "/CV/static/js/runtime-main.2622315c.js"
+  }
+]);
