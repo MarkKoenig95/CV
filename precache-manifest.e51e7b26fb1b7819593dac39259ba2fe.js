6bccab50a5440c4c0e8e4bafc7725709e7a0bd69
“Mark <markkoenig95@gmail.com>
2021-04-07 21:43:56 +0800

    Updates

---
+++ precache-manifest.e51e7b26fb1b7819593dac39259ba2fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d9d53db2823c3a3b7e063326ac3d16",
+    "url": "/www.markkoenig.dev/index.html"
+  },
+  {
+    "revision": "cc137fad4def65eabdd9",
+    "url": "/www.markkoenig.dev/static/css/main.4e719fb6.chunk.css"
+  },
+  {
+    "revision": "9a3acf05390ef599bf13",
+    "url": "/www.markkoenig.dev/static/js/2.e69f3464.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/www.markkoenig.dev/static/js/2.e69f3464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc137fad4def65eabdd9",
+    "url": "/www.markkoenig.dev/static/js/main.5dbcba60.chunk.js"
+  },
+  {
+    "revision": "594019c6939f477ed331",
+    "url": "/www.markkoenig.dev/static/js/runtime-main.2868d68e.js"
+  }
+]);
